06f39c96437c36d2bfe8ab111c1526862a0a7aa8
Binh Dau <dau.thanh.binh@gmail.com>
2023-06-30 20:44:10 +0700

    update xresource

--- home/theme.nix
+++ home/theme.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, theme, ... }: {
 
   home.pointerCursor = {
     x11.enable = true;
@@ -39,6 +39,27 @@
       scaling-factor = lib.hm.gvariant.mkUint32 2;
     };
   };
+
+  xresources.extraConfig = with theme.colors; ''
+    *background: ${background}
+    *foreground: ${foreground}
+    *color0: ${color0}
+    *color1: ${color1}
+    *color2: ${color2}
+    *color3: ${color3}
+    *color4: ${color4}
+    *color5: ${color5}
+    *color6: ${color6}
+    *color7: ${color7}
+    *color8: ${color8}
+    *color9: ${color9}
+    *color10: ${color10}
+    *color11: ${color11}
+    *color12: ${color12}
+    *color13: ${color13}
+    *color14: ${color14}
+    *color15: ${color15}
+  '';
 
   home.sessionVariables.GTK_THEME = "Adwaita";
 }
