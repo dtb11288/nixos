fc81c3fa516a7247b53529ba25e005afa14ce78b
Binh Dau <dau.thanh.binh@gmail.com>
2023-06-30 21:59:05 +0700

    add anti flicker for xps15

--- system/xps15.nix
+++ system/xps15.nix
@@ -48,14 +48,17 @@ in
     touchpad.disableWhileTyping = true;
   };
 
-  hardware.nvidia.prime = {
-    offload.enable = true;
+  hardware.nvidia = {
+    forceFullCompositionPipeline = true;
+    prime = {
+      offload.enable = true;
 
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:0:2:0";
+      # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+      intelBusId = "PCI:0:2:0";
 
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:1:0:0";
+      # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 
   environment.systemPackages = with pkgs; [
