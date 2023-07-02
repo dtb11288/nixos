84eb81f3cd3082e46b83404cf5158ad2a5623bd2
Binh Dau <dau.thanh.binh@gmail.com>
2023-07-02 16:33:39 +0700

    move config to common file

--- home/sxhkd-pc.nix
+++ home/sxhkd-pc.nix
@@ -1,23 +1,5 @@
 { pkgs, ... }:
 {
-  # Raises volume
-  XF86AudioRaiseVolume = "${pkgs.pamixer}/bin/pamixer -u -i 5";
-
-  # Lowers volume
-  XF86AudioLowerVolume = "${pkgs.pamixer}/bin/pamixer -d 5";
-
-  # Mute
-  XF86AudioMute = "${pkgs.pamixer}/bin/pamixer -t";
-
-  # Audio previeous
-  XF86AudioPrev = "${pkgs.playerctl}/bin/playerctl previous";
-
-  # Audio next
-  XF86AudioNext = "${pkgs.playerctl}/bin/playerctl next";
-
-  # Audio play
-  XF86AudioPlay = "${pkgs.playerctl}/bin/playerctl play-pause";
-
   # Brightness up
   XF86MonBrightnessUp = "${pkgs.ddcutil}/bin/ddcutil setvcp 10 + 10";
 
--- home/sxhkd.nix
+++ home/sxhkd.nix
@@ -6,9 +6,15 @@ in
   services.sxhkd = {
     enable = true;
     keybindings = with pkgs; keybindings // {
+      XF86AudioRaiseVolume = "${pkgs.pamixer}/bin/pamixer -u -i 5";
+      XF86AudioLowerVolume = "${pkgs.pamixer}/bin/pamixer -d 5";
+      XF86AudioMute = "${pkgs.pamixer}/bin/pamixer -t";
+      XF86AudioPrev = "${pkgs.playerctl}/bin/playerctl previous";
+      XF86AudioNext = "${pkgs.playerctl}/bin/playerctl next";
+      XF86AudioPlay = "${pkgs.playerctl}/bin/playerctl play-pause";
       "mod4 + p" = "rofi -show run";
       "mod4 + e" = "rofi -show emoji";
-      "mod4 + c" = "rofi -show calc -calc-command '${pkgs.xdotool}/bin/xdotool type --clearmodifiers {result}'";
+      "mod4 + c" = "rofi -show calc -calc-command '${pkgs.xsel}/bin/xsel -b'";
       "mod4 + s" = "${rofi-rbw}/bin/rofi-rbw";
     };
   };
