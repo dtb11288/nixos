f02c3a72d2647cbf531ac1715e7858984a0fb56d
Binh Dau <dau.thanh.binh@gmail.com>
2023-07-17 21:38:46 +0700

    correct rofi package

--- home/sxhkd.nix
+++ home/sxhkd.nix
@@ -1,6 +1,7 @@
 { pkgs, hostname, ... }:
 let
   keybindings = import ./sxhkd-${hostname}.nix { inherit pkgs; };
+  rofi-wrapped = with pkgs; rofi.override { plugins = [ rofi-calc rofi-emoji ]; };
 in
 {
   services.sxhkd = {
@@ -12,9 +13,9 @@ in
       XF86AudioPrev = "${pkgs.playerctl}/bin/playerctl previous";
       XF86AudioNext = "${pkgs.playerctl}/bin/playerctl next";
       XF86AudioPlay = "${pkgs.playerctl}/bin/playerctl play-pause";
-      "mod4 + p" = "rofi -show run";
-      "mod4 + e" = "rofi -show emoji";
-      "mod4 + c" = "rofi -show calc -no-show-match -no-sort -calc-command \"echo -n '\\\{result\\\}' | xsel -b\"";
+      "mod4 + p" = "${rofi-wrapped}/bin/rofi -show run";
+      "mod4 + e" = "${rofi-wrapped}/bin/rofi -show emoji";
+      "mod4 + c" = "${rofi-wrapped}/bin/rofi -show calc -no-show-match -no-sort -calc-command \"echo -n '\\\{result\\\}' | xsel -b\"";
       "mod4 + s" = "${rofi-rbw}/bin/rofi-rbw";
       "mod4 + shift + Return" = "${alacritty}/bin/alacritty";
       "mod4 + Escape" = "slock";
