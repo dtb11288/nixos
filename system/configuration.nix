5532209dad59c377ce62e952c4a92a80ef7a77ee
Binh DT <dau.thanh.binh@gmail.com>
2024-06-02 09:36:43 +0700

    use id for nextdns config

--- system/configuration.nix
+++ system/configuration.nix
@@ -121,10 +121,10 @@
     llmnr = "false";
     dnsovertls = "true";
     extraConfig = ''
-      DNS=45.90.28.0#${secrets.nextdns.domain}
-      DNS=2a07:a8c0::#${secrets.nextdns.domain}
-      DNS=45.90.30.0#${secrets.nextdns.domain}
-      DNS=2a07:a8c1::#${secrets.nextdns.domain}
+      DNS=45.90.28.0#${secrets.nextdns.id}.dns.nextdns.io
+      DNS=2a07:a8c0::#${secrets.nextdns.id}.dns.nextdns.io
+      DNS=45.90.30.0#${secrets.nextdns.id}.dns.nextdns.io
+      DNS=2a07:a8c1::#${secrets.nextdns.id}.dns.nextdns.io
     '';
   };
 
