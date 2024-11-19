b1708b45ebab711b9d2e739ccfdf11e97dd0fb45
Binh DT <dau.thanh.binh@gmail.com>
2024-11-19 09:31:51 +0700

    add flathub by default

--- system/xsession.nix
+++ system/xsession.nix
@@ -86,6 +86,13 @@
   services.blueman.enable = true;
 
   services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
   xdg.portal = {
     enable = true;
     config.common.default = "*";
