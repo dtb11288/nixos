52ada9605bd2d37fa12a93b38288f3442cae6ba9
Binh DT <dau.thanh.binh@gmail.com>
2024-11-19 11:52:24 +0700

    use udev file instead adding to extra rule

--- system/8bitdo.nix
+++ system/8bitdo.nix
@@ -4,12 +4,18 @@
   # Inspired by https://aur.archlinux.org/packages/8bitdo-ultimate-controller-udev
   environment.systemPackages = [ pkgs.xboxdrv ];
   # Udev rules to start or stop systemd service when controller is connected or disconnected
-  services.udev.extraRules = ''
-    # May vary depending on your controller model, find product id using 'lsusb'
-    SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3106", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl start 8bitdo-ultimate-xinput@2dc8:3106"
-    # This device (2dc8:3016) is "connected" when the above device disconnects
-    SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3016", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl stop 8bitdo-ultimate-xinput@2dc8:3106"
-  '';
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "8bitdo";
+      text = ''
+        # May vary depending on your controller model, find product id using 'lsusb'
+        SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3106", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl start 8bitdo-ultimate-xinput@2dc8:3106"
+        # This device (2dc8:3016) is "connected" when the above device disconnects
+        SUBSYSTEM=="usb", ATTR{idVendor}=="2dc8", ATTR{idProduct}=="3016", ATTR{manufacturer}=="8BitDo", RUN+="${pkgs.systemd}/bin/systemctl stop 8bitdo-ultimate-xinput@2dc8:3106"
+      '';
+      destination = "/etc/udev/rules.d/99-8bitdo.rules";
+    })
+  ];
   # Systemd service which starts xboxdrv in xbox360 mode
   systemd.services."8bitdo-ultimate-xinput@" = {
     unitConfig.Description = "8BitDo Ultimate Controller XInput mode xboxdrv daemon";
