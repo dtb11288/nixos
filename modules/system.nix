d22948eb01fb8595159fe8a20bfdd74c6faba560
Binh DT <dau.thanh.binh@gmail.com>
2025-03-31 09:33:26 +0700

    revert to auto freq

--- modules/system.nix
+++ modules/system.nix
@@ -123,20 +123,19 @@
 
   services.blueman.enable = true;
   services.gvfs.enable = true;
-  services.tlp.enable = true;
-  # services.auto-cpufreq = {
-  #   enable = true;
-  #   settings = {
-  #     battery = {
-  #        governor = "powersave";
-  #        turbo = "never";
-  #     };
-  #     charger = {
-  #        governor = "performance";
-  #        turbo = "auto";
-  #     };
-  #   };
-  # };
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+         governor = "powersave";
+         turbo = "never";
+      };
+      charger = {
+         governor = "performance";
+         turbo = "auto";
+      };
+    };
+  };
 
   environment.variables = {
     PATH = "/var/lib/flatpak/exports/bin:$PATH";
